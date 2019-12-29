2b58a4ea976229b644ac8c7545e43f3c9683cd47
Fernando Farias <fernnadolol1337@gmail.com>
2019-12-29 13:39:53 -0200

    criando as classes UI, chessmatch, chesspiece e o enum das cores

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Board;
+import chess.ChessMatch;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		ChessMatch m = new ChessMatch();
+		UI.printBoard(m.getPieces());
 	}
 
 }
